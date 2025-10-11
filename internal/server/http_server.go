bd90ed58daf12e8fb3914b8b7d7b3cb18b57c0a5
crazywh1t3 <32772327+crazywh1t3@users.noreply.github.com>
2025-10-11 10:07:33 +0300

    muling fixes

--- internal/server/http_server.go
+++ internal/server/http_server.go
@@ -1504,3 +1504,4 @@ func (s *HttpServer) resetDroplogs(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(map[string]any{"status": "ok", "dir": dir, "removed": removed})
 }
+
