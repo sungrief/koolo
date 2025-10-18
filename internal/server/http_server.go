91b2bfabb95588fd0d990bc1ce04c4e778da288f
EternalNooblet <EternalNooblet#8617>
2025-10-18 13:34:06 -0400

    Fix checkbox names for 'Buff on New Area' in character settings and templates

--- internal/server/http_server.go
+++ internal/server/http_server.go
@@ -1219,28 +1219,37 @@ func (s *HttpServer) characterSettings(w http.ResponseWriter, r *http.Request) {
 
 		cfg.Game.StonyTomb.OpenChests = r.Form.Has("gameStonytombOpenChests")
 		cfg.Game.StonyTomb.FocusOnElitePacks = r.Form.Has("gameStonytombFocusOnElitePacks")
-		cfg.Game.StonyTomb.BuffOnNewArea = r.Form.Has("gameStonyTombBuffOnNewArea")
+		cfg.Game.StonyTomb.BuffOnNewArea = r.Form.Has("gameStonytombBuffOnNewArea")
+
 		cfg.Game.AncientTunnels.OpenChests = r.Form.Has("gameAncientTunnelsOpenChests")
 		cfg.Game.AncientTunnels.FocusOnElitePacks = r.Form.Has("gameAncientTunnelsFocusOnElitePacks")
+
 		cfg.Game.Duriel.UseThawing = r.Form.Has("gameDurielUseThawing")
+
 		cfg.Game.Mausoleum.OpenChests = r.Form.Has("gameMausoleumOpenChests")
 		cfg.Game.Mausoleum.FocusOnElitePacks = r.Form.Has("gameMausoleumFocusOnElitePacks")
 		cfg.Game.Mausoleum.BuffOnNewArea = r.Form.Has("gameMausoleumBuffOnNewArea")
+
 		cfg.Game.DrifterCavern.OpenChests = r.Form.Has("gameDrifterCavernOpenChests")
 		cfg.Game.DrifterCavern.FocusOnElitePacks = r.Form.Has("gameDrifterCavernFocusOnElitePacks")
+
 		cfg.Game.SpiderCavern.OpenChests = r.Form.Has("gameSpiderCavernOpenChests")
 		cfg.Game.SpiderCavern.FocusOnElitePacks = r.Form.Has("gameSpiderCavernFocusOnElitePacks")
 		cfg.Game.SpiderCavern.BuffOnNewArea = r.Form.Has("gameSpiderCavernBuffOnNewArea")
+
 		cfg.Game.ArachnidLair.OpenChests = r.Form.Has("gameArachnidLairOpenChests")
 		cfg.Game.ArachnidLair.FocusOnElitePacks = r.Form.Has("gameArachnidLairFocusOnElitePacks")
+
 		cfg.Game.Mephisto.KillCouncilMembers = r.Form.Has("gameMephistoKillCouncilMembers")
 		cfg.Game.Mephisto.OpenChests = r.Form.Has("gameMephistoOpenChests")
 		cfg.Game.Mephisto.ExitToA4 = r.Form.Has("gameMephistoExitToA4")
 		cfg.Game.Mephisto.BuffOnNewArea = r.Form.Has("gameMephistoBuffOnNewArea")
+
 		cfg.Game.Tristram.ClearPortal = r.Form.Has("gameTristramClearPortal")
 		cfg.Game.Tristram.FocusOnElitePacks = r.Form.Has("gameTristramFocusOnElitePacks")
 		cfg.Game.Tristram.OnlyFarmRejuvs = r.Form.Has("gameTristramOnlyFarmRejuvs")
 		cfg.Game.Tristram.BuffOnNewArea = r.Form.Has("gameTristramBuffOnNewArea")
+
 		cfg.Game.Nihlathak.ClearArea = r.Form.Has("gameNihlathakClearArea")
 
 		cfg.Game.Baal.KillBaal = r.Form.Has("gameBaalKillBaal")
@@ -1251,8 +1260,10 @@ func (s *HttpServer) characterSettings(w http.ResponseWriter, r *http.Request) {
 
 		cfg.Game.Eldritch.KillShenk = r.Form.Has("gameEldritchKillShenk")
 		cfg.Game.Eldritch.BuffOnNewArea = r.Form.Has("gameEldritchBuffOnNewArea")
+
 		cfg.Game.LowerKurastChest.OpenRacks = r.Form.Has("gameLowerKurastChestOpenRacks")
 		cfg.Game.LowerKurastChest.BuffOnNewArea = r.Form.Has("gameLowerKurastChestBuffOnNewArea")
+
 		cfg.Game.Diablo.StartFromStar = r.Form.Has("gameDiabloStartFromStar")
 		cfg.Game.Diablo.KillDiablo = r.Form.Has("gameDiabloKillDiablo")
 		cfg.Game.Diablo.FocusOnElitePacks = r.Form.Has("gameDiabloFocusOnElitePacks")
@@ -1270,6 +1281,7 @@ func (s *HttpServer) characterSettings(w http.ResponseWriter, r *http.Request) {
 			}
 			cfg.Game.Diablo.AttackFromDistance = attackFromDistance
 		}
+
 		cfg.Game.Leveling.EnsurePointsAllocation = r.Form.Has("gameLevelingEnsurePointsAllocation")
 		cfg.Game.Leveling.EnsureKeyBinding = r.Form.Has("gameLevelingEnsureKeyBinding")
 		cfg.Game.Leveling.AutoEquip = r.Form.Has("gameLevelingAutoEquip")
