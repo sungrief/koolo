8206d0c7cd1708f4324009cc10bdddc9743ed844
Polivox <vampiresungrief@gmail.com>
2025-11-10 20:30:30 +0200

    Update screenshots
    Sorry for the commit, seems branching i got a non working version of the screenshotting. This is a cleaning up commit to trim to the working method.

--- internal/game/screenshot.go
+++ internal/game/screenshot.go
@@ -1,89 +1,94 @@
 package game
 
 import (
-	"image"
-	"reflect"
-	"unsafe"
+    "image"
+    "reflect"
+    "unsafe"
 
-	"github.com/hectorgimenez/koolo/internal/utils/winproc"
+    "github.com/hectorgimenez/koolo/internal/utils/winproc"
 )
 
 type bmpInfoHeader struct {
-	BiSize          uint32
-	BiWidth         int32
-	BiHeight        int32
-	BiPlanes        uint16
-	BiBitCount      uint16
-	BiCompression   uint32
-	BiSizeImage     uint32
-	BiXPelsPerMeter int32
-	BiYPelsPerMeter int32
-	BiClrUsed       uint32
-	BiClrImportant  uint32
+    BiSize          uint32
+    BiWidth         int32
+    BiHeight        int32
+    BiPlanes        uint16
+    BiBitCount      uint16
+    BiCompression   uint32
+    BiSizeImage     uint32
+    BiXPelsPerMeter int32
+    BiYPelsPerMeter int32
+    BiClrUsed       uint32
+    BiClrImportant  uint32
 }
 
-type bitmapInfo struct { Header bmpInfoHeader }
+type bitmapInfo struct{ Header bmpInfoHeader }
 
 type rect struct{ Left, Top, Right, Bottom int32 }
 
-type point struct{ X, Y int32 }
-
-func clientRectScreen(hwnd uintptr) (left, top, width, height int) {
-	var rc rect
-	winproc.GetClientRect.Call(hwnd, uintptr(unsafe.Pointer(&rc)))
-	pt := point{X: rc.Left, Y: rc.Top}
-	winproc.ClientToScreen.Call(hwnd, uintptr(unsafe.Pointer(&pt)))
-	w := int(rc.Right - rc.Left)
-	h := int(rc.Bottom - rc.Top)
-	return int(pt.X), int(pt.Y), w, h
+func clientSize(hwnd uintptr) (width, height int) {
+    var rc rect
+    winproc.GetClientRect.Call(hwnd, uintptr(unsafe.Pointer(&rc)))
+    return int(rc.Right - rc.Left), int(rc.Bottom - rc.Top)
 }
 
 func (gd *MemoryReader) Screenshot() image.Image {
-	gd.updateWindowPositionData()
-	left, top, width, height := clientRectScreen(uintptr(gd.HWND))
-	if width <= 0 || height <= 0 { return nil }
+    gd.updateWindowPositionData()
 
-	hdcScreen, _, _ := winproc.GetDC.Call(0)
-	if hdcScreen == 0 { return nil }
-	defer winproc.ReleaseDC.Call(0, hdcScreen)
+    width, height := clientSize(uintptr(gd.HWND))
+    if width <= 0 || height <= 0 {
+        return nil
+    }
 
-	hdcMem, _, _ := winproc.CreateCompatibleDC.Call(hdcScreen)
-	if hdcMem == 0 { return nil }
-	defer winproc.DeleteDC.Call(hdcMem)
+    // Screen DC (for compatibility); no blit from it, only to create DIB/DC
+    hdcScreen, _, _ := winproc.GetDC.Call(0)
+    if hdcScreen == 0 {
+        return nil
+    }
+    defer winproc.ReleaseDC.Call(0, hdcScreen)
 
-	bi := bitmapInfo{Header: bmpInfoHeader{BiSize: 40, BiWidth: int32(width), BiHeight: -int32(height), BiPlanes: 1, BiBitCount: 32}}
-	var bitsPtr uintptr
-	hbm, _, _ := winproc.CreateDIBSection.Call(hdcScreen, uintptr(unsafe.Pointer(&bi)), 0, uintptr(unsafe.Pointer(&bitsPtr)), 0, 0)
-	if hbm == 0 || bitsPtr == 0 { return nil }
-	defer winproc.DeleteObject.Call(hbm)
-	winproc.SelectObject.Call(hdcMem, hbm)
+    // Memory DC
+    hdcMem, _, _ := winproc.CreateCompatibleDC.Call(hdcScreen)
+    if hdcMem == 0 {
+        return nil
+    }
+    defer winproc.DeleteDC.Call(hdcMem)
 
-	for _, flags := range []uintptr{3, 2, 0} {
-		_, _, _ = winproc.PrintWindow.Call(uintptr(gd.HWND), hdcMem, flags)
-		winproc.GdiFlush.Call()
-		if img := dibToRGBA(bitsPtr, width, height); img != nil { return img }
-	}
+    // Top‑down 32‑bpp DIB
+    bi := bitmapInfo{Header: bmpInfoHeader{
+        BiSize:     40,
+        BiWidth:    int32(width),
+        BiHeight:   -int32(height),
+        BiPlanes:   1,
+        BiBitCount: 32,
+    }}
+    var bitsPtr uintptr
+    hbm, _, _ := winproc.CreateDIBSection.Call(hdcScreen, uintptr(unsafe.Pointer(&bi)), 0, uintptr(unsafe.Pointer(&bitsPtr)), 0, 0)
+    if hbm == 0 || bitsPtr == 0 {
+        return nil
+    }
+    defer winproc.DeleteObject.Call(hbm)
+    winproc.SelectObject.Call(hdcMem, hbm)
 
-	winproc.BitBlt.Call(hdcMem, 0, 0, uintptr(width), uintptr(height), hdcScreen, uintptr(left), uintptr(top), uintptr(winproc.SRCCOPY))
-	winproc.GdiFlush.Call()
-	return dibToRGBA(bitsPtr, width, height)
-}
+    // Single capture method: PrintWindow with PW_CLIENTONLY|PW_RENDERFULLCONTENT (flags=3)
+    _, _, _ = winproc.PrintWindow.Call(uintptr(gd.HWND), hdcMem, 3)
+    winproc.GdiFlush.Call()
 
-func dibToRGBA(bitsPtr uintptr, width, height int) *image.RGBA {
-	if bitsPtr == 0 || width <= 0 || height <= 0 { return nil }
-	n := width * height * 4
-	var src []byte
-	hdr := (*reflect.SliceHeader)(unsafe.Pointer(&src))
-	hdr.Data = bitsPtr
-	hdr.Len = n
-	hdr.Cap = n
-	img := image.NewRGBA(image.Rect(0, 0, width, height))
-	copy(img.Pix, src)
-	for y := 0; y < img.Bounds().Dy(); y++ {
-		for x := 0; x < img.Bounds().Dx(); x++ {
-			idx := y*img.Stride + x*4
-			img.Pix[idx], img.Pix[idx+2] = img.Pix[idx+2], img.Pix[idx]
-		}
-	}
-	return img
+    // Wrap the DIB memory into an RGBA and swap B<->R (BGRA->RGBA)
+    n := width * height * 4
+    var src []byte
+    hdr := (*reflect.SliceHeader)(unsafe.Pointer(&src))
+    hdr.Data = bitsPtr
+    hdr.Len = n
+    hdr.Cap = n
+
+    img := image.NewRGBA(image.Rect(0, 0, width, height))
+    copy(img.Pix, src)
+    for y := 0; y < img.Bounds().Dy(); y++ {
+        for x := 0; x < img.Bounds().Dx(); x++ {
+            idx := y*img.Stride + x*4
+            img.Pix[idx], img.Pix[idx+2] = img.Pix[idx+2], img.Pix[idx]
+        }
+    }
+    return img
 }
--- internal/utils/dpi_aware.go
+++ internal/utils/dpi_aware.go
@@ -3,5 +3,5 @@ package utils
 import "github.com/hectorgimenez/koolo/internal/utils/winproc"
 
 func init() {
-	winproc.SetProcessDpiAware.Call()
+    winproc.SetProcessDpiAware.Call()
 }
