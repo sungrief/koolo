26c263fd7d2a2e6c48824f0b3fe8c6ae0e83efc5
IronMaXxX <MaxHummel@gmx.de>
2026-02-01 21:22:07 +0100

    Implement thawing potion management for leveling characters before Duriel fight

--- internal/run/duriel.go
+++ internal/run/duriel.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"slices"
+	"time"
 
 	"github.com/hectorgimenez/d2go/pkg/data"
 	"github.com/hectorgimenez/d2go/pkg/data/area"
@@ -25,8 +26,10 @@ import (
 )
 
 const (
-	maxOrificeAttempts = 10
-	orificeCheckDelay  = 200
+	maxOrificeAttempts    = 10
+	orificeCheckDelay     = 200
+	thawingPotionsToDrink = 10 // Number of thawing potions consumed (counts separately for the character and the mercenary)
+	thawingMinBuffSeconds = 100
 )
 
 var talTombs = []area.ID{area.TalRashasTomb1, area.TalRashasTomb2, area.TalRashasTomb3, area.TalRashasTomb4, area.TalRashasTomb5, area.TalRashasTomb6, area.TalRashasTomb7}
@@ -104,6 +107,61 @@ func (d Duriel) Run(parameters *RunParameters) error {
 		}
 	}
 
+	_, isLevelingChar := d.ctx.Char.(context.LevelingCharacter)
+	useThawing := d.ctx.CharacterCfg.Game.Duriel.UseThawing || isLevelingChar
+	var thawingBuffDeadline time.Time
+	thawingBuffRemaining := func() time.Duration {
+		if thawingBuffDeadline.IsZero() {
+			return 0
+		}
+		return time.Until(thawingBuffDeadline)
+	}
+	updateThawingBuff := func(selfCount int) {
+		if selfCount <= 0 {
+			return
+		}
+		now := time.Now()
+		if thawingBuffDeadline.IsZero() || thawingBuffDeadline.Before(now) {
+			thawingBuffDeadline = now
+		}
+		thawingBuffDeadline = thawingBuffDeadline.Add(time.Duration(selfCount*30) * time.Second)
+	}
+	ensureThawingBuff := func(minSeconds int) error {
+		if !useThawing {
+			return nil
+		}
+		if minSeconds == 0 {
+			if !thawingBuffDeadline.IsZero() && thawingBuffRemaining() > 0 {
+				return nil
+			}
+		} else if thawingBuffRemaining() >= time.Duration(minSeconds)*time.Second {
+			return nil
+		}
+		if err := action.ReturnTown(); err != nil {
+			return err
+		}
+		if err := action.WayPoint(area.LutGholein); err != nil {
+			return err
+		}
+		mercAlive := d.ctx.Data.MercHPPercent() > 0
+		if err := d.buyAndDrinkThawingPotions(mercAlive, updateThawingBuff); err != nil {
+			return err
+		}
+		return action.UsePortalInTown()
+	}
+
+	// Get thawing potions at the start of the run (Lysander in Lut Gholein).
+	if useThawing {
+		if err := action.WayPoint(area.LutGholein); err != nil {
+			return err
+		}
+
+		mercAlive := d.ctx.Data.MercHPPercent() > 0
+		if err := d.buyAndDrinkThawingPotions(mercAlive, updateThawingBuff); err != nil {
+			return err
+		}
+	}
+
 	err := action.WayPoint(area.CanyonOfTheMagi)
 	if err != nil {
 		return err
@@ -115,7 +173,14 @@ func (d Duriel) Run(parameters *RunParameters) error {
 		return err
 	}
 
-	err = action.MoveToArea(realTalRashaTomb)
+	if isLevelingChar && d.ctx.Data.PlayerUnit.Area == area.CanyonOfTheMagi {
+		originalClearPathDist := d.ctx.CharacterCfg.Character.ClearPathDist
+		d.ctx.CharacterCfg.Character.ClearPathDist = 0
+		err = action.MoveToArea(realTalRashaTomb)
+		d.ctx.CharacterCfg.Character.ClearPathDist = originalClearPathDist
+	} else {
+		err = action.MoveToArea(realTalRashaTomb)
+	}
 	if err != nil {
 		return err
 	}
@@ -142,7 +207,11 @@ func (d Duriel) Run(parameters *RunParameters) error {
 	}
 
 	// Move to orifice and clear the area
-	err = action.MoveToCoords(orifice.Position)
+	moveOptions := []step.MoveOption{}
+	if d.ctx.Data.CanTeleport() {
+		moveOptions = append(moveOptions, step.WithIgnoreMonsters())
+	}
+	err = action.MoveToCoords(orifice.Position, moveOptions...)
 	if err != nil {
 		return err
 	}
@@ -172,14 +241,43 @@ func (d Duriel) Run(parameters *RunParameters) error {
 			utils.Sleep(500)
 			d.ctx.HID.Click(game.LeftButton, ui.AnvilBtnX, ui.AnvilBtnY)
 		}
-		utils.Sleep(20000)
+		if isLevelingChar {
+			d.ctx.Logger.Info("Waiting for Duriel's lair to open, securing area.")
+			nextClear := time.Now()
+			deadline := time.Now().Add(20 * time.Second)
+			portalOpened := false
+			for time.Now().Before(deadline) {
+				d.ctx.RefreshGameData()
+				if _, found := d.ctx.Data.Objects.FindOne(object.DurielsLairPortal); found {
+					portalOpened = true
+					break
+				}
+				if time.Now().After(nextClear) {
+					if err := action.ClearAreaAroundPlayer(12, data.MonsterAnyFilter()); err != nil {
+						return err
+					}
+					nextClear = time.Now().Add(2 * time.Second)
+				}
+				utils.Sleep(300)
+			}
+			if !portalOpened {
+				d.ctx.Logger.Warn("Duriel's lair did not open after waiting", "timeoutSeconds", 20)
+			}
+		} else {
+			utils.Sleep(20000)
+		}
 	}
 
-	_, isLevelingChar := d.ctx.Char.(context.LevelingCharacter)
 	if isLevelingChar && d.ctx.CharacterCfg.Game.Difficulty != difficulty.Hell {
 		action.ClearAreaAroundPlayer(20, data.MonsterAnyFilter())
 		if err := action.InRunReturnTownRoutine(); err != nil {
 			return err
+		}
+		if useThawing && thawingBuffRemaining() < time.Duration(thawingMinBuffSeconds)*time.Second {
+			d.ctx.Logger.Info("Refreshing thawing potions after town routine", "remainingSeconds", int(thawingBuffRemaining().Seconds()))
+			if err := ensureThawingBuff(thawingMinBuffSeconds); err != nil {
+				return err
+			}
 		}
 	}
 
@@ -188,80 +286,12 @@ func (d Duriel) Run(parameters *RunParameters) error {
 		return err
 	}
 
-	// Get thawing potions before entering Duriel's lair
-	if d.ctx.CharacterCfg.Game.Duriel.UseThawing || isLevelingChar {
-		d.ctx.Logger.Info("Returning to town for thawing potions before Duriel")
-
-		// Use the existing portal from the previous town prep
-		err = action.ReturnTown()
-		if err != nil {
-			return err
-		}
-
-		potsToBuy := 6
-		if d.ctx.Data.MercHPPercent() > 0 && !d.ctx.CharacterCfg.HidePortraits {
-			potsToBuy = 12
-		}
-
-		// Explicitly interact with Lysander for thawing potions
-		d.ctx.Logger.Info("Buying thawing potions from Lysander")
-		err = action.InteractNPC(npc.Lysander)
-		if err != nil {
-			return err
-		}
-
-		err = action.BuyAtVendor(npc.Lysander, action.VendorItemRequest{
-			Item:     "ThawingPotion",
-			Quantity: potsToBuy,
-			Tab:      4,
-		})
-		if err != nil {
-			return err
-		}
-
-		d.ctx.HID.PressKeyBinding(d.ctx.Data.KeyBindings.Inventory)
-		utils.Sleep(300)
-
-		x := 0
-		for _, itm := range d.ctx.Data.Inventory.ByLocation(item.LocationInventory) {
-			if itm.Name != "ThawingPotion" {
-				continue
-			}
-			pos := ui.GetScreenCoordsForItem(itm)
-			utils.Sleep(500)
-
-			if x > 5 {
-				d.ctx.HID.Click(game.LeftButton, pos.X, pos.Y)
-				utils.Sleep(300)
-				if d.ctx.Data.LegacyGraphics {
-					d.ctx.HID.Click(game.LeftButton, ui.MercAvatarPositionXClassic, ui.MercAvatarPositionYClassic)
-				} else {
-					d.ctx.HID.Click(game.LeftButton, ui.MercAvatarPositionX, ui.MercAvatarPositionY)
-				}
-			} else {
-				d.ctx.HID.Click(game.RightButton, pos.X, pos.Y)
-			}
-			x++
-		}
-		step.CloseAllMenus()
-
-		// Go back through the portal
-		d.ctx.Logger.Info("Returning through portal to Duriel's entrance")
-		err = action.UsePortalInTown()
-		if err != nil {
-			return err
-		}
-
-		// Move back to the orifice area
-		for _, obj := range d.ctx.Data.Areas[realTalRashaTomb].Objects {
-			if obj.Name == object.HoradricOrifice {
-				action.MoveToCoords(obj.Position)
-			}
-		}
-	}
-
 	d.ctx.RefreshGameData()
 	utils.Sleep(200)
+
+	if err := ensureThawingBuff(0); err != nil {
+		return err
+	}
 
 	duriellair, found := d.ctx.Data.Objects.FindOne(object.DurielsLairPortal)
 	if found {
@@ -310,6 +340,73 @@ func (d Duriel) Run(parameters *RunParameters) error {
 	return nil
 }
 
+func (d Duriel) drinkThawingPotions(selfTarget, mercTarget int) (int, int) {
+	mercAlive := d.ctx.Data.MercHPPercent() > 0
+	if selfTarget < 0 {
+		selfTarget = 0
+	}
+	if mercTarget < 0 {
+		mercTarget = 0
+	}
+	if !mercAlive {
+		mercTarget = 0
+	}
+	if selfTarget == 0 && mercTarget == 0 {
+		return 0, 0
+	}
+	shouldGiveMerc := mercAlive && mercTarget > 0
+	reHidePortraits := false
+	if shouldGiveMerc && d.ctx.CharacterCfg.HidePortraits && !d.ctx.Data.OpenMenus.PortraitsShown {
+		d.ctx.CharacterCfg.HidePortraits = false
+		reHidePortraits = true
+		d.ctx.HID.PressKey(d.ctx.Data.KeyBindings.ShowPortraits.Key1[0])
+		utils.Sleep(200)
+	}
+
+	d.ctx.HID.PressKeyBinding(d.ctx.Data.KeyBindings.Inventory)
+	utils.Sleep(300)
+
+	selfCount := 0
+	mercCount := 0
+
+	for _, itm := range d.ctx.Data.Inventory.ByLocation(item.LocationInventory) {
+		if itm.Name != "ThawingPotion" {
+			continue
+		}
+		pos := ui.GetScreenCoordsForItem(itm)
+		utils.Sleep(500)
+
+		if selfCount < selfTarget {
+			d.ctx.HID.Click(game.RightButton, pos.X, pos.Y)
+			selfCount++
+			continue
+		}
+
+		if mercCount < mercTarget {
+			d.ctx.HID.Click(game.LeftButton, pos.X, pos.Y)
+			utils.Sleep(300)
+			if d.ctx.Data.LegacyGraphics {
+				d.ctx.HID.Click(game.LeftButton, ui.MercAvatarPositionXClassic, ui.MercAvatarPositionYClassic)
+			} else {
+				d.ctx.HID.Click(game.LeftButton, ui.MercAvatarPositionX, ui.MercAvatarPositionY)
+			}
+			mercCount++
+			continue
+		}
+
+		d.ctx.HID.Click(game.RightButton, pos.X, pos.Y)
+		selfCount++
+	}
+	step.CloseAllMenus()
+
+	if reHidePortraits {
+		d.ctx.CharacterCfg.HidePortraits = true
+		_ = action.HidePortraits()
+	}
+
+	return selfCount, mercCount
+}
+
 func (d Duriel) findRealTomb() (area.ID, error) {
 	var realTomb area.ID
 
@@ -327,6 +424,118 @@ func (d Duriel) findRealTomb() (area.ID, error) {
 	}
 
 	return realTomb, nil
+}
+
+func (d Duriel) countFreeInventorySlots() int {
+	occupied := [4][10]bool{}
+	for _, i := range d.ctx.Data.Inventory.ByLocation(item.LocationInventory) {
+		w := i.Desc().InventoryWidth
+		h := i.Desc().InventoryHeight
+		for y := 0; y < h; y++ {
+			for x := 0; x < w; x++ {
+				if i.Position.Y+y < 4 && i.Position.X+x < 10 {
+					occupied[i.Position.Y+y][i.Position.X+x] = true
+				}
+			}
+		}
+	}
+
+	for y, row := range d.ctx.CharacterCfg.Inventory.InventoryLock {
+		if y < 4 {
+			for x, cell := range row {
+				if x < 10 && cell == 0 {
+					occupied[y][x] = true
+				}
+			}
+		}
+	}
+
+	free := 0
+	for y := 0; y < 4; y++ {
+		for x := 0; x < 10; x++ {
+			if !occupied[y][x] {
+				free++
+			}
+		}
+	}
+	return free
+}
+
+func (d Duriel) buyAndDrinkThawingPotions(mercAlive bool, updateTimer func(int)) error {
+	selfTarget := thawingPotionsToDrink
+	mercTarget := 0
+	if mercAlive {
+		mercTarget = thawingPotionsToDrink
+	}
+
+	drinkAndLog := func(selfTarget, mercTarget int) (int, int) {
+		selfCount, mercCount := d.drinkThawingPotions(selfTarget, mercTarget)
+		if updateTimer != nil {
+			updateTimer(selfCount)
+		}
+		d.ctx.Logger.Info("Thawing potions consumed", "self", selfCount, "merc", mercCount)
+		return selfCount, mercCount
+	}
+
+	selfCount, mercCount := drinkAndLog(selfTarget, mercTarget)
+	selfTarget -= selfCount
+	mercTarget -= mercCount
+	if selfTarget < 0 {
+		selfTarget = 0
+	}
+	if mercTarget < 0 {
+		mercTarget = 0
+	}
+
+	for selfTarget > 0 || mercTarget > 0 {
+		freeSlots := d.countFreeInventorySlots()
+		if freeSlots == 0 {
+			d.ctx.Logger.Warn("Not enough inventory space to buy thawing potions", "free", freeSlots, "required", selfTarget+mercTarget)
+			return nil
+		}
+
+		batch := min(freeSlots, selfTarget+mercTarget)
+		if err := d.buyThawingPotions(batch); err != nil {
+			return err
+		}
+		d.ctx.RefreshGameData()
+
+		selfCount, mercCount = drinkAndLog(selfTarget, mercTarget)
+		if selfCount == 0 && mercCount == 0 {
+			d.ctx.Logger.Warn("Failed to consume thawing potions after purchase", "remaining", selfTarget+mercTarget)
+			return nil
+		}
+
+		selfTarget -= selfCount
+		mercTarget -= mercCount
+		if selfTarget < 0 {
+			selfTarget = 0
+		}
+		if mercTarget < 0 {
+			mercTarget = 0
+		}
+	}
+
+	return nil
+}
+
+func (d Duriel) buyThawingPotions(quantity int) error {
+	if quantity <= 0 {
+		return nil
+	}
+	d.ctx.Logger.Info("Buying thawing potions from Lysander", "quantity", quantity)
+	if err := action.InteractNPC(npc.Lysander); err != nil {
+		return err
+	}
+
+	if err := action.BuyAtVendor(npc.Lysander, action.VendorItemRequest{
+		Item:     "ThawingPotion",
+		Quantity: quantity,
+		Tab:      4,
+	}); err != nil {
+		return err
+	}
+	return step.CloseAllMenus()
 }
 
 func (d Duriel) prepareStaff() error {
