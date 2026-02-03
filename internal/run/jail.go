09a0ec8732fdf63eaa253e3005a556be34168ab9
Genqila <102831561+genqila@users.noreply.github.com>
2026-02-03 16:20:19 +0700

    feat(run): improve bone_ash, cave and jail to use preset unit

--- internal/run/jail.go
+++ internal/run/jail.go
@@ -4,6 +4,7 @@ import (
 	"github.com/hectorgimenez/d2go/pkg/data"
 	"github.com/hectorgimenez/d2go/pkg/data/area"
 	"github.com/hectorgimenez/d2go/pkg/data/npc"
+	"github.com/hectorgimenez/d2go/pkg/data/superunique"
 	"github.com/hectorgimenez/koolo/internal/action"
 	"github.com/hectorgimenez/koolo/internal/config"
 	"github.com/hectorgimenez/koolo/internal/context"
@@ -48,33 +49,63 @@ func (j Jail) Run(parameters *RunParameters) error {
 		return err
 	}
 
-	action.ItemPickup(30)
-
 	if err := action.MoveToArea(area.JailLevel1); err != nil {
 		return err
 	}
 
-	return action.MoveToArea(area.Barracks)
+	if err := action.MoveToArea(area.Barracks); err != nil {
+		return err
+	}
+
+	// In order to prevent interact between exit and TP portal if we do any
+	j.ctx.PathFinder.RandomMovement()
+
+	return nil
 }
 
 func (j Jail) killPitspawn() error {
-	areaData, ok := j.ctx.Data.Areas[area.JailLevel2]
-	if ok {
-		if npcData, found := areaData.NPCs.FindOne(npc.Tainted); found && len(npcData.Positions) > 0 {
-			if err := action.MoveToCoords(npcData.Positions[0]); err != nil {
-				j.ctx.Logger.Warn("Jail run: failed moving to Pitspawn Fouldog", "error", err)
-			}
-		} else {
-			j.ctx.Logger.Warn("Jail run: Pitspawn Fouldog position not found")
-		}
-	} else {
-		j.ctx.Logger.Warn("Jail run: map data missing for Jail Level 2")
+	monsterPosition := data.Position{}
+	if npcData, found := j.ctx.Data.NPCs.FindOneBySuperUniqueID(superunique.PitspawnFouldog); found {
+		monsterPosition = npcData.Positions[0]
 	}
 
-	return j.ctx.Char.KillMonsterSequence(func(d game.Data) (data.UnitID, bool) {
+	if monsterPosition == (data.Position{}) {
+		j.ctx.Logger.Warn("Jail run: super unique not found, exploring area")
+		if err := action.ClearCurrentLevelEx(true, data.MonsterAnyFilter(), func() bool {
+			if monster, found := j.ctx.Data.Monsters.FindOne(npc.Tainted, data.MonsterTypeSuperUnique); found {
+				monsterPosition = monster.Position
+				j.ctx.Logger.Warn("Jail run: super unique found during exploration")
+				return true
+			}
+
+			return false
+		}); err != nil {
+			return err
+		}
+		if monsterPosition == (data.Position{}) {
+			j.ctx.Logger.Warn("Jail run: super unique not found after exploration")
+		}
+	}
+
+	if monsterPosition != (data.Position{}) {
+		if err := action.MoveToCoords(monsterPosition); err != nil {
+			return err
+		}
+	}
+
+	if err := j.ctx.Char.KillMonsterSequence(func(d game.Data) (data.UnitID, bool) {
 		if m, found := d.Monsters.FindOne(npc.Tainted, data.MonsterTypeSuperUnique); found {
 			return m.UnitID, true
 		}
+
 		return 0, false
-	}, nil)
+	}, nil); err != nil {
+		return err
+	}
+
+	if err := action.ItemPickup(30); err != nil {
+		return err
+	}
+
+	return nil
 }
